6527c21115e5654cbfec850e6162a21f348ebf06
alexandrenavarro <alexandre.j.navarro@gmail.com>
2023-02-19 23:35:12 +0100

    Init checkstyle-sample project

---
+++ src/main/java/com/github/alexandrenavarro/checkstylesample/Main.java
@@ -0,0 +1,18 @@
+package com.github.alexandrenavarro.checkstylesample;
+
+
+public final class Main {
+
+    private Main() {
+    }
+
+    public static void main(final String[] args) {
+        if (true) {
+            System.out.println("Hello word");
+        }
+
+
+    }
+
+
+}
